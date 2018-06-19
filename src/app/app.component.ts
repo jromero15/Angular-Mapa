6dfc2f3dbd86e8225f5c578b0c7c305b6c3c341b
unknown <jromero0527@gmail.com>
2018-06-18 19:43:57 -0500

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'My first AGM project';
+  latitude: number = 4.7109886;
+  longitude: number = -74.072092;
+
+  onChoseLocation(event) {  
+    this.latitude = event.coords.lat;
+    this.longitude = event.coords.lng;
+  }
 }
